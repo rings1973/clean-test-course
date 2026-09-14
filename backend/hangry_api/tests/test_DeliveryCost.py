452dfc5b0b71fdd3f2568c740a75b9d2335f5b05
rings1973 <jeremy.ringhoff@gmail.com>
2026-09-13 21:11:55 -0400

    Update default delivery fee

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,12 +1,13 @@
 from api.controllers import Delivery
-from django_mock_queries.query import MockSet, MockModel
+from types import SimpleNamespace
 
 def test_LotsOfItems():
   #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
+  order = [
+    SimpleNamespace(quantity=5),
+    SimpleNamespace(quantity=5),
+    SimpleNamespace(quantity=5),
+  ]
   delivery_distance = 6
   #Act
   cost = Delivery.calculate(order,delivery_distance)
@@ -15,10 +16,11 @@ def test_LotsOfItems():
 
 def test_MiddleOfTheRoadItems():
   #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
+  order = [
+    SimpleNamespace(quantity=2),
+    SimpleNamespace(quantity=2),
+    SimpleNamespace(quantity=2),
+  ]
   delivery_distance = 4
   #Act
   cost = Delivery.calculate(order,delivery_distance)
@@ -27,9 +29,9 @@ def test_MiddleOfTheRoadItems():
 
 def test_LittleItems():
   #Arrange
-  # TODO: Arrange the items to run the test
+  order = [SimpleNamespace(quantity=2)]
+  delivery_distance = 1
   #Act
-  # TODO: Call the function that will be tested
+  cost = Delivery.calculate(order, delivery_distance)
   #Assert
-  # TODO: replace the pass with an assert to test the value returned.
-  pass
+  assert cost == 3.50
